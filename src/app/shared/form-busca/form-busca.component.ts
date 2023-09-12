34284dea744f061e8bed4a5cf4c5fbc3a591dfc1
CST2083 <marcelo.barbosa@b2card.com.br>
2023-09-12 20:16:21 -0300

    atualização do template

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    });
+  }
+}
