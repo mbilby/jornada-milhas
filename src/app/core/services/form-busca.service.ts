7ab2d9e71a2d8d2734c057114c0eff5faa12b197
CST2083 <marcelo.barbosa@b2card.com.br>
2023-09-15 17:55:57 -0300

    implementação da automação de seleção de classes

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatChipSelectionChange } from '@angular/material/chips';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormBuscaService {
   formBusca: FormGroup;
-  constructor() { 
+  constructor(private dialog: MatDialog,) { 
     this.formBusca = new FormGroup({
-      somenteIda: new FormControl(false)
+      somenteIda: new FormControl(false),
+      tipo: new FormControl("Executiva"),
     })
   }
   obterControle(nome:string): FormControl {
@@ -17,5 +21,20 @@ export class FormBuscaService {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
+  }
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string) {
+    if(evento.selected){
+      this.formBusca.patchValue({
+        tipo,
+      })
+      console.log('tipo de passagem alterado para: ', tipo)
+    }
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    });
   }
 }
